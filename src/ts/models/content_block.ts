b85222a175a0091cace894a8be00882f0e366161
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-06-11 11:45:37 +0200

    simple content block class

---
+++ src/ts/models/content_block.ts
@@ -0,0 +1,11 @@
+///<reference path="../../../typings/tsd.d.ts"/>
+
+class ContentBlock {
+  id:number;
+  content:string;
+
+  constructor(id:number, content:string) {
+    this.id = id;
+    this.content = content;
+  }
+}
