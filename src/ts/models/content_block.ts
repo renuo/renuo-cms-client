df5210817019423f5d28f38b2d4a7c94e487a07b
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-06-11 17:23:15 +0200

    extended

--- src/ts/models/content_block.ts
+++ src/ts/models/content_block.ts
@@ -3,9 +3,11 @@
 class ContentBlock {
   id:number;
   content:string;
+  content_path:string;
 
-  constructor(id:number, content:string) {
+  constructor(id:number, content:string, content_path:string) {
     this.id = id;
     this.content = content;
+    this.content_path = content_path;
   }
 }
