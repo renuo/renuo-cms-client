9c5b47d8f10e905bc4bec87d522204cd7216bca6
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-06-11 17:22:58 +0200

    DI (not tested yet)

---
+++ src/ts/dependency_injection/dependency_injection_container.ts
@@ -0,0 +1,12 @@
+class DependencyInjectionContainer {
+  objects = {};
+
+  constructor() {
+    this.objects[AjaxService] = new AjaxService('url') // TODO: set per config class
+    this.objects[DomService] = new DomService()
+  }
+
+  get(className:any) {
+    return this.objects[className]
+  }
+}
