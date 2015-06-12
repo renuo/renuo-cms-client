ece58a7eb61073de83ff816c06a24106ebe5b806
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-06-12 17:26:05 +0200

    content block controller init

---
+++ src/ts/controllers/content_block_controller.ts
@@ -0,0 +1,10 @@
+///<reference path="../../../typings/tsd.d.ts"/>
+///<reference path="../dependency_injection/dependency_injection_container.ts"/>
+///<reference path="../services/dom_service.ts"/>
+///<reference path="../services/ajax_service.ts"/>
+///<reference path="../models/content_block.ts"/>
+
+
+class ContentBlockController {
+
+}
