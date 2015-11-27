db1c0b9ff9749425714d71e3de6dccfd53950c19
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-27 01:40:57 +0100

    gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,16 @@ var babel = require('gulp-babel');
 var tsProject = ts.createProject('./lib/tsconfig.json');
 
 gulp.task('default', function () {
-  return gulp.src('lib/**/*.ts')
+  return gulp.src('src/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('release'));
+});
+
+gulp.task('build', function () {
+  return gulp.src('src/**/*.ts')
     .pipe(sourcemaps.init())
     .pipe(ts(tsProject))
     .pipe(babel())
---
+++ src/content_block.ts
@@ -0,0 +1,10 @@
+///<reference path="../typings/tsd.d.ts"/>
+
+class ContentBlock {
+  content:string;
+  contentPath:string;
+  apiKey:string;
+
+  constructor(public content:string, public contentPath:string, public apiKey:string) {
+  }
+}
