afa9cfcaffec1312d45a361f09de2ac90f5e38ce
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-30 21:29:30 +0100

    refactoring

--- src/app/data_services/ajax_service.spec.ts
+++ src/app/data_services/ajax_service.spec.ts
@@ -6,9 +6,6 @@ describe('AjaxService', function () {
     return jQuery.Deferred().resolve(response).promise();
   }
 
-  const createdAt = new Date(2015, 11, 30);
-  const updatedAt = new Date(2015, 12, 3);
-
   const newContentBlock = {
     content_block: {
       api_key: 'api-key',
@@ -18,6 +15,9 @@ describe('AjaxService', function () {
       updated_at: 0
     }
   };
+
+  const createdAt = new Date(2015, 11, 30);
+  const updatedAt = new Date(2015, 12, 3);
 
   const existingContentBlock = {
     content_block: {
@@ -29,39 +29,41 @@ describe('AjaxService', function () {
     }
   };
 
-  it('Fetches a content block with the right request method', function () {
-    spyOn(jQuery, 'ajax').and.callFake(function (request:any) {
-      expect(request.url).toBe('/v1/api-key/content_blocks/my-path');
-      expect(request.type).toBe('get');
-      expect(request.dataType).toBe('json');
-      return ajax_response(newContentBlock);
+  describe('#fetch', function () {
+    it('Fetches a content block with the right request method', function () {
+      spyOn(jQuery, 'ajax').and.callFake(function (request:any) {
+        expect(request.url).toBe('/v1/api-key/content_blocks/my-path');
+        expect(request.type).toBe('get');
+        expect(request.dataType).toBe('json');
+        return ajax_response(newContentBlock);
+      });
+      const service = new AjaxService('api-key');
+      service.fetch('my-path').then((result) => {
+      });
     });
-    const service = new AjaxService('api-key');
-    service.fetch('my-path').then((result) => {
-    });
-  });
 
-  it('Fetches a new content block', function () {
-    spyOn(jQuery, 'ajax').and.returnValue(ajax_response(newContentBlock));
-    const service = new AjaxService('api-key');
-    service.fetch('my-path').then((result) => {
-      expect(result.content_block.api_key).toBe('api-key');
-      expect(result.content_block.content_path).toBe('my-path');
-      expect(result.content_block.content).toBe('');
-      expect(result.content_block.created_at).toBeFalsy();
-      expect(result.content_block.updated_at).toBeFalsy();
+    it('Fetches a new content block', function () {
+      spyOn(jQuery, 'ajax').and.returnValue(ajax_response(newContentBlock));
+      const service = new AjaxService('api-key');
+      service.fetch('my-path').then((result) => {
+        expect(result.content_block.api_key).toBe('api-key');
+        expect(result.content_block.content_path).toBe('my-path');
+        expect(result.content_block.content).toBe('');
+        expect(result.content_block.created_at).toBeFalsy();
+        expect(result.content_block.updated_at).toBeFalsy();
+      });
     });
-  });
 
-  it('Fetches an existing content block', function () {
-    spyOn(jQuery, 'ajax').and.returnValue(ajax_response(existingContentBlock));
-    const service = new AjaxService('api-key');
-    service.fetch('my-path').then((result) => {
-      expect(result.content_block.api_key).toBe('api-key');
-      expect(result.content_block.content_path).toBe('my-path');
-      expect(result.content_block.content).toBe('some content');
-      expect(result.content_block.created_at).toBe(createdAt);
-      expect(result.content_block.updated_at).toBe(updatedAt);
+    it('Fetches an existing content block', function () {
+      spyOn(jQuery, 'ajax').and.returnValue(ajax_response(existingContentBlock));
+      const service = new AjaxService('api-key');
+      service.fetch('my-path').then((result) => {
+        expect(result.content_block.api_key).toBe('api-key');
+        expect(result.content_block.content_path).toBe('my-path');
+        expect(result.content_block.content).toBe('some content');
+        expect(result.content_block.created_at).toBe(createdAt);
+        expect(result.content_block.updated_at).toBe(updatedAt);
+      });
     });
   });
 });
