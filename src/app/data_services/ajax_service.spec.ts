b84540e7b3f4aca7f4602a5caa60d07e5cafa445
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-30 21:56:24 +0100

    implement store content block

--- src/app/data_services/ajax_service.spec.ts
+++ src/app/data_services/ajax_service.spec.ts
@@ -30,22 +30,22 @@ describe('AjaxService', function () {
   };
 
   describe('#fetchContentBlock', function () {
-    it('Fetches a content block with the right request method', function () {
+    it('fetches a content block with the right request method', function () {
       spyOn(jQuery, 'ajax').and.callFake(function (request:any) {
         expect(request.url).toBe('/v1/api-key/content_blocks/my-path');
         expect(request.type).toBe('get');
         expect(request.dataType).toBe('json');
         return ajax_response(newContentBlock);
       });
-      const service = new AjaxService('api-key');
-      service.fetchContentBlock('my-path').then((result) => {
+      const service = new AjaxService();
+      service.fetchContentBlock('api-key', 'my-path').then(() => {
       });
     });
 
-    it('Fetches a new content block', function () {
+    it('fetches a new content block', function () {
       spyOn(jQuery, 'ajax').and.returnValue(ajax_response(newContentBlock));
-      const service = new AjaxService('api-key');
-      service.fetchContentBlock('my-path').then((result) => {
+      const service = new AjaxService();
+      service.fetchContentBlock('api-key', 'my-path').then((result) => {
         expect(result.content_block.api_key).toBe('api-key');
         expect(result.content_block.content_path).toBe('my-path');
         expect(result.content_block.content).toBe('');
@@ -54,15 +54,33 @@ describe('AjaxService', function () {
       });
     });
 
-    it('Fetches an existing content block', function () {
+    it('fetches an existing content block', function () {
       spyOn(jQuery, 'ajax').and.returnValue(ajax_response(existingContentBlock));
-      const service = new AjaxService('api-key');
-      service.fetchContentBlock('my-path').then((result) => {
+      const service = new AjaxService();
+      service.fetchContentBlock('api-key', 'my-path').then((result) => {
         expect(result.content_block.api_key).toBe('api-key');
         expect(result.content_block.content_path).toBe('my-path');
         expect(result.content_block.content).toBe('some content');
         expect(result.content_block.created_at).toBe(createdAt);
         expect(result.content_block.updated_at).toBe(updatedAt);
+      });
+    });
+  });
+
+  describe('#storeContentBlock', function () {
+    it('stores a content block on the server', function () {
+      spyOn(jQuery, 'ajax').and.callFake(function (request:any) {
+        expect(request.url).toBe('/v1/my-api-key/content_blocks');
+        expect(request.type).toBe('POST');
+        expect(request.dataType).toBe('json');
+        const parsed = JSON.parse(request.data);
+        expect(parsed.content_block.content).toBe('content');
+        expect(parsed.content_block.content_path).toBe('path');
+        expect(parsed.content_block.api_key).toBe('my-api-key');
+        return ajax_response(newContentBlock);
+      });
+      const service = new AjaxService();
+      service.storeContentBlock(new ContentBlock('content', 'path', 'my-api-key')).then(() => {
       });
     });
   });
--- src/app/data_services/ajax_service.ts
+++ src/app/data_services/ajax_service.ts
@@ -1,8 +1,21 @@
 class AjaxService {
-  constructor(private apiKey:string) {
+  fetchContentBlock(apiKey:string, contentPath:string) {
+    return jQuery.getJSON(`/v1/${apiKey}/content_blocks/${contentPath}`);
   }
 
-  fetchContentBlock(contentPath:string) {
-    return jQuery.getJSON(`/v1/${this.apiKey}/content_blocks/${contentPath}`);
+  storeContentBlock(contentBlock:ContentBlock) {
+    return jQuery.ajax({
+      url: `/v1/${contentBlock.apiKey}/content_blocks`,
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'POST',
+      data: JSON.stringify({
+        content_block: {
+          content: contentBlock.content,
+          content_path: contentBlock.contentPath,
+          api_key: contentBlock.apiKey
+        }
+      })
+    });
   }
 }
