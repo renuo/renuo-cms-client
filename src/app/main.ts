f229c6fb85cfa07c34117688b2d52876dab5253d
Lukas Elmer <lukas.elmer@gmail.com>
2015-12-02 21:25:41 +0100

    test loading in real browser

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,11 @@
 ///<reference path="data/models/content_block.ts"/>
 ///<reference path="data/services/ajax_service.ts"/>
 ///<reference path="data/services/data_service.ts"/>
+///<reference path="views/editors/ckeditor/ckeditor_loader.ts"/>
 
 const service = new DataService(new AjaxService('http://renuo-cms-api.dev:3000'));
 const content = service.loadContent(new ContentBlock('', 'some-path', 'my-path'));
 let c = console;
 content.then((x) => c.log(x));
+const loader = new CkeditorLoader();
+loader.loadEditor().then(() => c.log(CKEDITOR.version));
