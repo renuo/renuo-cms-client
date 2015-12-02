2d2638017db9b80eadd0aa11cec3e9a7037a597e
Lukas Elmer <lukas.elmer@gmail.com>
2015-12-02 21:25:24 +0100

    change to full version of ckeditor

--- src/app/views/editors/ckeditor/ckeditor_loader.ts
+++ src/app/views/editors/ckeditor/ckeditor_loader.ts
@@ -1,7 +1,7 @@
 ///<reference path="../../../../../typings/jquery/jquery.d.ts"/>
 
 class CkeditorLoader {
-  loadEditor(editorCdnUrl:string = '//cdn.ckeditor.com/4.5.5/standard/ckeditor.js'):JQueryXHR {
+  loadEditor(editorCdnUrl:string = '//cdn.ckeditor.com/4.5.5/full/ckeditor.js'):JQueryXHR {
     return jQuery.getScript(editorCdnUrl);
   }
 }
