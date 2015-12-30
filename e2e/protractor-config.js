a294fd58036fd90a752ac5521f3a9009971694b7
Lukas Elmer <lukas.elmer@gmail.com>
2015-12-31 00:32:50 +0100

    change chrome to firefox

--- e2e/protractor-config.js
+++ e2e/protractor-config.js
@@ -1,7 +1,6 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
   specs: ['todo-spec.js']
 };
