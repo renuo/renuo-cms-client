8bccc2099873ddf91879c7aa3a4d0db96061b491
Lukas Elmer <lukas.elmer@gmail.com>
2016-01-28 11:28:08 +0100

    renuo upload typings

--- src/app/views/editors/upload/renuo_upload.d.ts
+++ src/app/views/editors/upload/renuo_upload.d.ts
@@ -1,5 +1,19 @@
+interface RenuoUploadCallback {
+  (event:RenuoUploadEvent): void;
+}
+
+interface RenuoUploadEvent {
+  orginalName:string;
+  cleanName:string;
+  name:string;
+  extension:string;
+  size:string;
+  publicUrl:string;
+  filePath:string;
+}
+
 interface RenuoUploadStatic {
-  (element:HTMLElement|JQuery, options:any, ...callback:any[]): void;
+  (element:HTMLElement|JQuery, options:any, ...callback:RenuoUploadCallback[]): void;
   version:number;
 }
 
