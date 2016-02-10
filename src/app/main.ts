14a5ff29d0c8f31a1b2a1433b57585c80aff3805
Lukas Elmer <lukas.elmer@gmail.com>
2016-02-10 11:20:43 +0100

    better reload strategy (todo: test it)

--- src/app/main.ts
+++ src/app/main.ts
@@ -24,7 +24,10 @@
       )
     );
     controller.init();
+
+    jQuery(document).on('renuo-cms-reload', function () {
+      controller.init();
+    });
   };
   jQuery(initContentBlocks);
-  jQuery(document).on('renuo-cms-reload', initContentBlocks);
 })();
