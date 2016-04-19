ae3902f65919137ce49ccd09c1fc839b93f139ba
Cyril Kyburz <cyril.kyburz@renuo.ch>
2016-04-19 18:07:51 +0200

    add expiry date for the local storage

--- src/app/data/services/local_storage_service.ts
+++ src/app/data/services/local_storage_service.ts
@@ -1,11 +1,26 @@
 ///<reference path="../models/content_block.ts"/>
 
 class LocalStorageService {
+  static EXPIRATIONTIME = 5; //minutes
+  private expiryDate:{[key: string]: Date} = {};
+
   fetch(key:string):AjaxContentBlocksHash {
     return JSON.parse(localStorage.getItem(key));
   }
 
   put(key:string, hash:AjaxContentBlocksHash) {
+    if (this.isValid(key)) return;
+    this.setExpiryDate(key);
     localStorage.setItem(key, JSON.stringify(hash));
+  }
+
+  private isValid(key: string) {
+    return this.expiryDate[key] - Date.now() > 0;
+  }
+
+  private setExpiryDate(key: string) {
+    const expiryDate = new Date;
+    expiryDate.setMinutes ( expiryDate.getMinutes() + LocalStorageService.EXPIRATIONTIME );
+    this.expiryDate[key] = expiryDate;
   }
 }
