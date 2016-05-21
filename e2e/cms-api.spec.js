8f102fbbea2bf8b0448dc89ee37959efbd722e03
Lukas Elmer <lukas.elmer@gmail.com>
2016-05-21 18:51:27 +0200

    increase sleep time to make e2e tests more solid on ci

--- e2e/cms-api.spec.js
+++ e2e/cms-api.spec.js
@@ -3,25 +3,27 @@
 describe('renuo cms api', function () {
   browser.ignoreSynchronization = true;
 
-  it('connects to the site and selects an element', function () {
+  beforeEach(function() {
     browser.get('http://localhost:8080/travis.html');
+  });
+
+  it('connects to the site and selects an element', function () {
     var el = element(by.css('[data-content-path="some/path/to/some/content"]'));
     expect(el.getAttribute('data-api-key')).toEqual('aValidApiKey');
   });
 
   it('loads some content', function () {
-    browser.get('http://localhost:8080/travis.html');
     var el = element(by.css('[data-content-path="a"]'));
     el.getText().then(function (text) {
       var newContent = text + 'x';
       expect(el.getText()).not.toEqual(newContent);
       el.click();
-      browser.sleep(500);
+      browser.sleep(5000);
       el.sendKeys('x');
       expect(el.getText()).toEqual(newContent);
       var el2 = element(by.css('[data-content-path="even-more-content"]'));
       el2.click();
-      browser.sleep(500);
+      browser.sleep(5000);
 
       browser.get('http://localhost:8080/travis.html?t=2');
       var newEl = element(by.css('[data-content-path="a"]'));
