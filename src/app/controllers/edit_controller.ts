fc69a91f5ca69fe00ba13c044f39421b95eb928d
Lukas Elmer <lukas.elmer@gmail.com>
2016-05-25 17:45:53 +0200

    fix naming issue

--- src/app/controllers/edit_controller.ts
+++ src/app/controllers/edit_controller.ts
@@ -10,11 +10,11 @@ class EditController {
               private preparer:EditorPreparer) {
   }
 
-  prepareEdit(dom:DomContentBlock):void {
+  prepareEdit(domOuter:DomContentBlock):void {
     this.loadDependencies();
 
     this.loadingCallback.done(() =>
-      this.preparer.prepare(dom, (domB:DomContentBlock, newContent:string) => this.editContent(domB, newContent)));
+      this.preparer.prepare(domOuter, (dom:DomContentBlock, newContent:string) => this.editContent(dom, newContent)));
   }
 
   editContent(dom:DomContentBlock, newContent:string) {
