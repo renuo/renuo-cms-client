433f1e7440d014992c72494ba23f5b89b0ce682a
Lukas Elmer <lukas.elmer@gmail.com>
2016-06-02 13:45:08 +0200

    upgrade typings to 1.x.x, update typing files

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/ckeditor/index.d.ts" />
-/// <reference path="browser/ambient/dropzone/index.d.ts" />
-/// <reference path="browser/ambient/jasmine-ajax/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/jquery/index.d.ts" />
