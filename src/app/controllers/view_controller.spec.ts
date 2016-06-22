30c6f710d2233f6a48cb7d689f6393ff68be8bcd
Zora Fuchs <zora.fuchs@renuo.ch>
2016-06-22 11:56:28 +0200

    - put duplicated constants out of the tests and have them in describe section - move mocks to beforeEach method, as far as possible

--- src/app/controllers/view_controller.spec.ts
+++ src/app/controllers/view_controller.spec.ts
@@ -3,30 +3,26 @@
 ///<reference path="../views/services/content_block_finder.ts"/>
 ///<reference path="../data/models/content_block.ts"/>
 ///<reference path="../views/models/dom_content_block.ts"/>
+///<reference path="../views/services/dom_content_block_converter.ts"/>
 
 describe('ViewController', function () {
-  it('initializes the contents', function () {
-    const elements:HTMLElement[] = [
-      jQuery('<div data-content-path="my-path" data-api-host="host" data-api-key="my-key"></div>')[0],
-      jQuery('<div data-content-path="editable-path" data-api-host="host" data-api-key="my-key" data-private-api-key="PK"></div>')[0]
-    ];
+  const elements:HTMLElement[] = [
+    jQuery('<div data-content-path="my-path" data-api-host="host" data-api-key="my-key"></div>')[0],
+    jQuery('<div data-content-path="editable-path" data-api-host="host" data-api-key="my-key" data-private-api-key="PK"></div>')[0]
+  ];
+  const finder = new ContentBlockFinder();
+  const converter = new DomContentBlockConverter();
+  const dataService = new DataService(null);
+  const drawer = new ContentBlockDrawer();
+  const editController = new EditController(null, null, null, null);
+  const controller = new ViewController(finder, converter, dataService, drawer, editController);
 
-    const finder = new ContentBlockFinder();
-    const converter = new DomContentBlockConverter();
-    const dataService = new DataService(null);
-    const drawer = new ContentBlockDrawer();
-    const editController = new EditController(null, null, null, null);
-    const controller = new ViewController(finder, converter, dataService, drawer, editController);
+  const domContent1:DomContentBlock = converter.convert(elements[0]);
+  const domContent2:DomContentBlock = converter.convert(elements[1]);
 
-    const domContent1:DomContentBlock = converter.convert(elements[0]);
-    const domContent2:DomContentBlock = converter.convert(elements[1]);
-
+  beforeEach(function() {
     spyOn(finder, 'find').and.returnValue(elements);
     spyOn(converter, 'convert').and.callThrough();
-    const editControllerSpy = spyOn(editController, 'prepareEdit');
-    editControllerSpy.and.callFake((dom:DomContentBlock) => {
-      expect(dom.contentBlock.contentPath).toBe('editable-path');
-    });
 
     spyOn(dataService, 'loadEditableContent').and.callFake((cb:ContentBlock) => {
       const d = new Date(2015, 11, 21);
@@ -45,7 +41,13 @@ describe('ViewController', function () {
       const d = new Date(2015, 11, 21);
       return new ContentBlock(`cached content ${cb.contentPath}`, cb.contentPath, cb.apiKey, cb.apiHost, d, d);
     });
+  });
 
+  it('initializes the contents', function () {
+    const editControllerSpy = spyOn(editController, 'prepareEdit');
+    editControllerSpy.and.callFake((dom:DomContentBlock) => {
+      expect(dom.contentBlock.contentPath).toBe('editable-path');
+    });
     const drawerSpy = spyOn(drawer, 'draw').and.stub();
 
     controller.init();
@@ -66,44 +68,9 @@ describe('ViewController', function () {
   });
 
   it('initializes the contents of specified content-blocks', function () {
-    const elements:HTMLElement[] = [
-      jQuery('<div data-content-path="my-path" data-api-host="host" data-api-key="my-key"></div>')[0],
-      jQuery('<div data-content-path="editable-path" data-api-host="host" data-api-key="my-key" data-private-api-key="PK"></div>')[0]
-    ];
-
-    const finder = new ContentBlockFinder();
-    const converter = new DomContentBlockConverter();
-    const dataService = new DataService(null);
-    const drawer = new ContentBlockDrawer();
-    const editController = new EditController(null, null, null, null);
-    const controller = new ViewController(finder, converter, dataService, drawer, editController);
-
-    const domContent1:DomContentBlock = converter.convert(elements[0]);
-    const domContent2:DomContentBlock = converter.convert(elements[1]);
-
-    spyOn(finder, 'find').and.returnValue(elements);
-    spyOn(converter, 'convert').and.callThrough();
     const editControllerSpy = spyOn(editController, 'prepareEdit');
     editControllerSpy.and.callFake((dom:DomContentBlock) => {
       expect(dom.contentBlock.contentPath).toBe('editable-path');
-    });
-
-    spyOn(dataService, 'loadEditableContent').and.callFake((cb:ContentBlock) => {
-      const d = new Date(2015, 11, 21);
-      const filledCb = new ContentBlock(`real content ${cb.contentPath}`, cb.contentPath, cb.apiKey, cb.apiHost, d, d);
-      const editableCb = new EditableContentBlock(filledCb, null);
-      return jQuery.Deferred().resolve(editableCb).promise();
-    });
-
-    spyOn(dataService, 'loadReadonlyContent').and.callFake((cb:ContentBlock) => {
-      const d = new Date(2015, 11, 21);
-      const filledCb = new ContentBlock(`real content ${cb.contentPath}`, cb.contentPath, cb.apiKey, cb.apiHost, d, d);
-      return jQuery.Deferred().resolve(filledCb).promise();
-    });
-
-    spyOn(dataService, 'loadReadonlyContentFromCache').and.callFake((cb:ContentBlock) => {
-      const d = new Date(2015, 11, 21);
-      return new ContentBlock(`cached content ${cb.contentPath}`, cb.contentPath, cb.apiKey, cb.apiHost, d, d);
     });
 
     const drawerSpy = spyOn(drawer, 'draw').and.stub();
