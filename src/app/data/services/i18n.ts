a57fb47137bd185459a36bdbbb8194eef07e26eb
Zora Fuchs <zora.fuchs@renuo.ch>
2016-07-11 11:43:11 +0200

    change wording again

--- src/app/data/services/i18n.ts
+++ src/app/data/services/i18n.ts
@@ -2,7 +2,7 @@ class I18n {
   private static translations:{[key:string]:string} = {
     'cms.edit.message.conflict': 'Could not save the content. A newer Version already exists on the server.',
     'cms.edit.message.unauthorized': 'Could not save the content. You are not authorized to change it.',
-    'cms.edit.message.no-connection': 'Could not save the content. You could not connect to the server.'
+    'cms.edit.message.no-connection': 'Could not save the content. No connection to the server.'
   };
 
   static t(key:string):string {
