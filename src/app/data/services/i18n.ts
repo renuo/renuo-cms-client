1993c90ee18b13a75a0c03fa13945f24aa400650
Zora Fuchs <zora.fuchs@renuo.ch>
2016-07-11 11:08:35 +0200

    add translations

--- src/app/data/services/i18n.ts
+++ src/app/data/services/i18n.ts
@@ -1,6 +1,8 @@
 class I18n {
   private static translations:{[key:string]:string} = {
-    'cms.edit.message.conflict': 'Could not save the content. A newer Version already exists on the server.'
+    'cms.edit.message.conflict': 'Could not save the content. A newer Version already exists on the server.',
+    'cms.edit.message.unauthorized': 'You are not authorized to change the content of this block.',
+    'cms.edit.message.no-connection': 'No connection to the server. Either you are offline or the server is down.'
   };
 
   static t(key:string):string {
